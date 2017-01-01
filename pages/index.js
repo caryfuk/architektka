777697e7e9b08e8cb7ad12739aaf7194f0388539
Pavel Moravec <pavelm@salsitasoft.com>
2017-01-01 22:35:35 +0100

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Helmet from 'react-helmet'
+import Slider from 'react-slick'
 import { config } from 'config'
 
 export default class Index extends React.Component {
@@ -19,6 +20,16 @@ export default class Index extends React.Component {
         <h1>
           Portfolio
         </h1>
+        <Slider
+          infinite
+          speed={500}
+          slidesToShow={1}
+          slidesToScroll={1}
+          arrows={false}
+        >
+          <div><img src={`/studies/solo/1200/picarch3a.jpg`} /></div>
+          <div><img src={`/studies/solo/1200/picarch3b.jpg`} /></div>
+        </Slider>
       </div>
     )
   }
