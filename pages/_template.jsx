1eca44e951e525aaa533eb254b78448392586026
Pavel Moravec <pavelm@salsitasoft.com>
2017-01-03 12:04:17 +0100

    new styles

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -15,16 +15,14 @@ module.exports = React.createClass({
     return (
       <div>
         <Headroom>
-          <div>
-            <Link to={prefixLink('/')}>Architektka</Link>
-            <ul>
-              <li><Link to={prefixLink('/houses-and-apartments/')}>Domy a byty</Link></li>
-              <li><Link to={prefixLink('/commercial/')}>Komerčné objekty</Link></li>
-              <li><Link to={prefixLink('/studies/')}>Štúdie</Link></li>
-              <li><Link to={prefixLink('/objects/')}>Objekty</Link></li>
-              <li><Link to={prefixLink('/blog/')}>Blog</Link></li>
-            </ul>
-          </div>
+          <Link to={prefixLink('/')}>Architektka</Link>
+          <ul>
+            <li><Link to={prefixLink('/houses-and-apartments/')}>Domy a byty</Link></li>
+            <li><Link to={prefixLink('/commercial/')}>Komerčné objekty</Link></li>
+            <li><Link to={prefixLink('/studies/')}>Štúdie</Link></li>
+            <li><Link to={prefixLink('/objects/')}>Objekty</Link></li>
+            <li><Link to={prefixLink('/blog/')}>Blog</Link></li>
+          </ul>
         </Headroom>
         <div className='content'>
           {this.props.children}
